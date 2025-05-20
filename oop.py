51976092c0c1ead35d0392e2b22386da9f81a906
moskalenkoalina <moskalenkoalina062@gmail.com>
2025-05-20 23:25:26 +0300

    Виправлення помилок

--- oop.py
+++ oop.py
@@ -6,25 +6,26 @@ class MemoryGame:
         self.root = root
         self.root.title("MEMORY GAME")
         self.root.configure(bg="White")
-        self.logic = Gamelogic()
+        self.logic = GameLogic()
         self.style = GameStyle()
         self.buttons = [[None] * 4 for _ in range(4)]
         self.create_header()
         self.build_grid()
         self.create_restart_button()
 
-    def create_heade(self):
-        title = tk.Label(self.root, text = "GAME CHALLENGE",
-                         font=('Comic Sans MS',25, 'bold'),
-                         bg = '#ADD8E6', fg = '#FF00FF')
+    def create_header(self):
+        title = tk.Label(self.root, text="GAME CHALLENGE",
+                         font=('Comic Sans MS', 25, 'bold'),
+                         bg='white', fg='#FF00FF')
         title.grid(row=0, column=0, columnspan=4, pady=(10, 20))
 
-    def build_grig(self):
+    def build_grid(self):
         for i in range(4):
             for j in range(4):
-                btn = tk.Button(self.root, command=lambda i=i, j=j: self.on_clik(i,j))
-                self.style.style_button = btn
+                btn = tk.Button(self.root, command=lambda i=i, j=j: self.on_click(i, j))
+                self.style.style_button(btn)
                 self.style.set_hidden(btn)
                 btn.grid(row=i + 1, column=j, padx=5, pady=5)
                 self.buttons[i][j] = btn
+
 
